7f91c44a8da86fb018d8b2e22264d4b25cafb48d
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-05-06 20:54:53 +0200

    Login form (not finished)

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="conatiner-fluid login-page">
+  <div class="container-fluid login-page">
     <div class="row">
-      <div class="col-6 image-background-login">
+      <div class="col-6 d-none d-lg-block image-background-login">
         <div
           class="color-overlay d-flex justify-content-center align-items-center"
           :style="{
@@ -9,8 +9,43 @@
           }"
         ></div>
       </div>
-      <div class="col-6 login-form">
-        <h1 style="color: red">asd</h1>
+      <div class="col-6 login-form text-center">
+        <div class="border rounded-5">
+          <div class="login">
+            <div class="header-login mt-5">
+              <h1>Minorify</h1>
+              <p>Connect. Share. Empower</p>
+            </div>
+            <div class="form mt-4">
+              <label for="email" class="text-start authentication">Email</label>
+              <div class="input-group mb-3 gap-3 ps-3 pe-3">
+                <input
+                  type="text"
+                  placeholder="Email..."
+                  class="form-control authentication"
+                  id="email"
+                />
+              </div>
+              <label for="password" class="authentication mt-3">Password</label>
+              <div class="input-group mb-3 gap-3 ps-3 pe-3">
+                <input
+                  type="text"
+                  placeholder="Password..."
+                  class="form-control authentication mb-3"
+                  id="password"
+                />
+              </div>
+              <div class="validation mb-3">
+                <button type="button" class="btn btn-outline-info me-4 w-25">
+                  Login
+                </button>
+                <button type="button" class="btn btn-outline-info w-25">
+                  Regiter
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -25,16 +60,21 @@ export default {
   },
   mounted() {
     //Choose Background Image
-    const images = [require("../assets/background-login-arabic.jpg")];
-    const index_number = Math.floor(Math.random() * (images.length - 1));
-    console.log("Random Number: " + index_number);
-    console.log("Images number: " + (images.length - 1));
-    this.image = images[index_number];
+    const images = [
+      require("../assets/background-login-thailand.jpg"),
+      require("../assets/background-login-senegal.jpg"),
+      require("../assets/background-login-turkey.jpg"),
+      require("../assets/background-login-mexico.jpg"),
+      require("../assets/background-login-india.jpg"),
+      require("../assets/background-login-china.jpg"),
+    ];
+    const index_number = Math.floor(Math.random() * images.length);
+    this.image = "url(" + images[index_number] + ")";
   },
 };
 </script>
 <style scoped>
 .image-background-login {
-  background-image: url("../assets/background-login-arabic.jpg") !important;
+  background-image: v-bind(image) !important;
 }
 </style>
