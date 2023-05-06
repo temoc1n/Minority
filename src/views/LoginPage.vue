b2c4aafc4123c94215d593e7543fb1e60f784207
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-05-06 17:40:41 +0200

    New features / Base

---
+++ src/views/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-6">
+        <h1>asd</h1>
+      </div>
+      <div class="col-6">
+        <h1>asd</h1>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "LoginPage",
+};
+</script>
+<style></style>
